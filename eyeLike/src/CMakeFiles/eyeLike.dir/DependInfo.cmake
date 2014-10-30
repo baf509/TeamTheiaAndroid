195171b3b8c697a3f7abcbfbfd141b2d3efff2e2
coolis <wayne.liwen@gmail.com>
2014-10-30 18:48:03 -0400

    add eyeLike

---
+++ eyeLike/src/CMakeFiles/eyeLike.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/findEyeCenter.cpp" "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/findEyeCorner.cpp" "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/helpers.cpp" "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/main.cpp" "/Users/WayneLI/Developer/simple_eye_gaze_tracker/eyeLike/src/CMakeFiles/eyeLike.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
